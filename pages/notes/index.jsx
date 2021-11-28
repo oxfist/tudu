48b0e1a007262193d6f678666dc5943147a9864a
Andrés Quilodrán <andres.caro.q@gmail.com>
2021-11-28 20:27:28 -0300

    feat: add CSS module for Notes Index Page

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
+import { title } from '../../styles/styles.module.css'
 
 const Page = () => (
   <div>
-    <h1>Note index path</h1>
+    <h1 className={title}>Note index path</h1>
     <Link href="/notes/[id]" as={`/notes/1`}>
       <a>Note 1</a>
     </Link>
