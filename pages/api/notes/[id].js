81151139368e5f943d2423c9f331cc79789eb498
Andrés Quilodrán <andres.quilodran@laboratoria.la>
2022-01-24 20:38:58 -0300

    feat: add PATCH and DELETE endpoints for notes with existence validation

--- pages/api/notes/[id].js
+++ pages/api/notes/[id].js
@@ -3,16 +3,50 @@ import notes from '../../../src/data/data'
 
 const NOT_FOUND = 404
 
-const handler = nc().get((req, res) => {
-  const note = notes.find((n) => parseInt(req.query.id) === n.id)
+const getNote = (id) => notes.find((n) => parseInt(id) === n.id)
+const getNoteIndex = (id) => notes.findIndex((n) => parseInt(id) === n.id)
 
-  if (!note) {
-    res.status(NOT_FOUND)
-    res.end()
-    return
-  }
+const handler = nc()
+  .get((req, res) => {
+    const note = getNote(req.query.id)
 
-  res.json({ data: note })
-})
+    if (!note) {
+      res.status(NOT_FOUND)
+      res.end()
+      return
+    }
+
+    res.json({ data: note })
+  })
+  .patch((req, res) => {
+    const note = getNote(req.query.id)
+
+    if (!note) {
+      res.status(NOT_FOUND)
+      res.end()
+      return
+    }
+
+    const i = getNoteIndex(req.query.id)
+    const updatedNote = { ...note, ...req.body }
+
+    notes[i] = updatedNote
+    res.json({ data: updatedNote })
+  })
+  .delete((req, res) => {
+    const note = getNote(req.query.id)
+
+    if (!note) {
+      res.status(NOT_FOUND)
+      res.end()
+      return
+    }
+
+    const i = getNoteIndex(req.query.id)
+
+    notes.splice(i, 1)
+
+    res.json({ data: re.query.id })
+  })
 
 export default handler
